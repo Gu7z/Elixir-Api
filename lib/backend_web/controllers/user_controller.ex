5316e7200871cf6ff22bc01b217ab038816731ba
gustavo <gustavoferri13@gmail.com>
2020-08-31 19:08:49 -0400

    adding new api

---
+++ lib/backend_web/controllers/user_controller.ex
@@ -0,0 +1,11 @@
+defmodule BackendWeb.UserController do
+  use BackendWeb, :controller
+
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+
+  def show(conn, %{"id" => id}) do
+    render(conn, "show.html", id: id)
+  end
+end
