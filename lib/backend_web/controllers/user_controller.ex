906f36e464ebde83acb4b79ff9f2076a36fdbae1
gustavo <gustavoferri13@gmail.com>
2020-08-31 20:18:35 -0400

    add json to request parameters

--- lib/backend_web/controllers/user_controller.ex
+++ lib/backend_web/controllers/user_controller.ex
@@ -1,11 +1,42 @@
 defmodule BackendWeb.UserController do
   use BackendWeb, :controller
+  require Logger
 
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
   def show(conn, %{"id" => id}) do
-    render(conn, "show.html", id: id)
+    json(conn, getUsers(id))
+  end
+
+  def getUsers(id) do
+    arrayOfMaps = [
+      %{
+        id: 1,
+        name: "gustavo"
+      },
+      %{
+        id: 2,
+        name: "elixir"
+      }
+    ]
+
+    Logger.info("Inicio")
+
+    querry =
+      Enum.find(arrayOfMaps, fn
+        elem ->
+          "#{elem.id}" === "#{id}"
+      end)
+
+    Logger.info("Querry response: #{querry[:name]}")
+
+    Logger.info("Fim")
+
+    cond do
+      querry[:name] -> %{name: querry[:name]}
+      !querry[:name] -> "Id invalido"
+    end
   end
 end
